1ee36419c6bfd78a74e40172464954c2af96df1c
TomVasile <43349666+TomVasile@users.noreply.github.com>
2020-02-14 18:20:49 +0000

    bump to 0.14.0

--- CasperLabs_Client.rb
+++ CasperLabs_Client.rb
@@ -1,9 +1,9 @@
 class CasperlabsClient < Formula
   desc "Scala client for the CasperLabs platform."
   homepage "www.casperlabs.io"
-  version "0.12.0"
+  version "0.14.0"
   url "https://github.com/CasperLabs/CasperLabs/releases/download/v#{version}/casperlabs-client-#{version}.tgz"
-  sha256 "d7c8728596ba3bdc2c8a5797024f003610e304084bc96be568b38c903dacbe12"
+  sha256 "3059672cf87945ba1746ece31277f3633012b5bfc3e8af6ea64f2f410ceb7586"
 
   JAVA_VERSION = "1.10+"
 
